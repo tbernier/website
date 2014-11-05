6c898f3b432268b1ad80c7c11909aa02f1b56d6a
Thierry Bernier <thierry.bernier@wikiogroup.com>
2014-11-05 15:58:55 +0100

    Add imagemin

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,9 +2,11 @@ module.exports = function(grunt) {
 
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-newer');
 
 	var jsSrc = 'src/js/script.js',
 		jsDist = 'dist/js/built.js';
@@ -44,6 +46,16 @@ module.exports = function(grunt) {
 				dest: jsDist
 			}
 		},
+		imagemin: {
+			dynamic: {
+				files: [{
+					expand: true,                  // Enable dynamic expansion
+					cwd: 'src/img/',                   // Src matches are relative to this path
+					src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+					dest: 'dist/img/'                  // Destination path prefix
+				}]
+			}
+		},
 		watch: {
 			options: {
 				livereload: '<%= connect.options.livereload %>'
@@ -61,6 +73,10 @@ module.exports = function(grunt) {
 			styles: {
 				files: '**/*.scss',
 				tasks: ['styles:dist']
+			},
+			imagemin: {
+				files: ['**/*.{png,jpg,gif}'],
+				tasks: ['imagemin']
 			}
 		},
 		connect: {
@@ -78,8 +94,8 @@ module.exports = function(grunt) {
 	});
 
 	grunt.registerTask('default', ['connect', 'dev', 'watch']);
-	grunt.registerTask('dev', ['styles:dev', 'scripts:dev']);
-	grunt.registerTask('dist', ['styles:dist', 'scripts:dist']);
+	grunt.registerTask('dev', ['styles:dev', 'scripts:dev', 'newer:imagemin']);
+	grunt.registerTask('dist', ['styles:dist', 'scripts:dist', 'newer:imagemin']);
 
 	grunt.registerTask('scripts:dev', ['concat:compile']);
 	grunt.registerTask('scripts:dist', ['uglify:compile']);
